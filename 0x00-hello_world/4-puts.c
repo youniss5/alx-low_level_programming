5224dca5d2b12e99eff6ea81b4739566af2d9f50
youniss5 <ahmedhyouniss@gmail.com>
2023-06-15 14:59:41 -0700

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main(void)/**
-*-main - main entry
+*main : main entry
 */
 {puts("\"Programming is like building a multilingual puzzle");
 return (0);
 /**
- *-return 0 = success
+ *return 0: means success
  */
 }
