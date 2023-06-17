7fe286d5f218b19535623f11878ca760db58031e
youniss5 <ahmedhyouniss@gmail.com>
2023-06-16 18:20:25 -0700

    comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point 
+ * Description: 'print combination'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x;
+for (x = 0; x <= 9; x++)
+{
+putchar((x % 10) + '0');
+if (x== 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+
+putchar('\n');
+
+return (0);
+}
