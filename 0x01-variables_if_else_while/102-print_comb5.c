5db8e5e428e4a260bd375af83e366ae111df5608
youniss5 <ahmedhyouniss@gmail.com>
2023-06-16 19:42:00 -0700

    combo

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 /**
-* main-Entry point 
-* Description:'2-2 digit num program'
-* Return: Always 0 (Success)
+*main-Entry point 
+*Description:'2-2 digit num program'
+*Return: Always 0 (Success)
 */
 int main(void)
 {
