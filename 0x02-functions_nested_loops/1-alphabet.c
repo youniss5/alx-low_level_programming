db7409073cadf8e9416e1e71b824b05324ae290e
youniss5 <ahmedhyouniss@gmail.com>
2023-06-20 11:20:36 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_alphabet - Entry poin
+* Description: 'Print alphabet onto the screen'
+*is written by youniss
+* Return: Always 0 (Success)
+*/
+void print_alphabet(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+_putchar('\n');
+}
