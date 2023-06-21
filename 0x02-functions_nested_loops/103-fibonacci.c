6811f7866a11332857056d5cc348eca65af79af1
youniss5 <ahmedhyouniss@gmail.com>
2023-06-20 18:29:25 -0700

    fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,14 +7,15 @@
 int main(void)
 {
 	int i = 0;
-	int j = 1;
-	int k = 2;
-	sum = k ;
+	long j = 1;
+	long k = 2;
+	long sum = k;
+
 	while (k + j < 4000000)
 	{
 		k += j;
-		if(k % 2 == 0)
-			sum +=k;
+		if (k % 2 == 0)
+			sum += k;
 		j = k - j;
 
 		++i;
