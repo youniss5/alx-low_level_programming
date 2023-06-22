eef5d3065c347cc0aface74ccc6054017e314519
youniss5 <ahmedhyouniss@gmail.com>
2023-06-22 14:39:55 -0700

    multiply 2 number

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
  *mul - multiplies 2 numbers
- *@c : char to be tested.
+ *@a: first number
+ *@b: second number
  *Return: 1 if its upper case and 0 if its not
  */
 int mul(int a, int b)
