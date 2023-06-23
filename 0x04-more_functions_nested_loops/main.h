fb6f5e14f3132d5df06cef9f33c79658900b0fa7
youniss5 <ahmedhyouniss@gmail.com>
2023-06-22 18:31:52 -0700

    print square

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef main_h
 #define main_h
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -8,5 +9,7 @@ void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
+void print_square(int size);
+int _putchar(char c);
 
 #endif
