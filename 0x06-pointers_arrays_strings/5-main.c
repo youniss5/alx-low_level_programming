dcda45c917eed0b0b847c0921512d1b563f9f555
youniss5 <ahmedhyouniss@gmail.com>
2023-06-29 12:46:13 -0700

    lower to upper case

--- 0x06-pointers_arrays_strings/5-main.c
+++ 0x06-pointers_arrays_strings/5-main.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+* main - check the code
+*
+* Return: Always 0.
+*/
 int main(void)
 {
-    char str[] = "Look up!\n";
-    char *ptr;
+char str[] = "Look up!\n";
+char *ptr;
 
-    ptr = string_toupper(str);
-    printf("%s", ptr);
-    printf("%s", str);
-    return (0);
+ptr = string_toupper(str);
+printf("%s", ptr);
+printf("%s", str);
+return (0);
 }
