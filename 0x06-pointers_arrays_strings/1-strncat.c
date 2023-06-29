2481007a6fe46f6c3a084ec2fac299951181592a
youniss5 <ahmedhyouniss@gmail.com>
2023-06-29 11:49:51 -0700

    concatenates strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * *_strcat - concatenates two strings using at most n bytes from src
+ * *_strncat - concatenates two strings using at most n bytes from src
  *@dest: input value
  *@src: input value
  *@n: input value
