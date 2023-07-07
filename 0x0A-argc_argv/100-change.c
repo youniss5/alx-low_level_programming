eb983afb6d66ca6c82e46b91291a97109b195e83
youniss5 <ahmedhyouniss@gmail.com>
2023-07-07 15:19:38 -0700

    change money

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point.
+ * @argc: integer
+ * @argv: array
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+if (argc == 2)
+{
+int i, leastcents = 0, money = atoi(argv[1]);
+int cents[] = {25, 10, 5, 2, 1};
+
+for (i = 0; i < 5; i++)
+{
+if (money >= cents[i])
+{
+leastcents += money / cents[i];
+money = money % cents[i];
+if (money % cents[i] == 0)
+{
+break;
+}
+}
+}
+printf("%d\n", leastcents);
+return (1);
+}
+else
+{
+printf("Error\n");
+return (0);
+}
+}
