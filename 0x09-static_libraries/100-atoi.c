f4105d9bc72bfb28172b60ec70684adc6d7f3b2a
youniss5 <ahmedhyouniss@gmail.com>
2023-07-07 18:32:25 -0700

    libmy

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -3,7 +3,7 @@
  * _atoi - converts a string to an integer
  * @s: string
  */
-int main(void)
+int _atoi(char *s)
 {
     _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
     return (0);
