be73eb3b30b83f72ccb86b024d0851f1aaf859e5
youniss5 <ahmedhyouniss@gmail.com>
2023-07-17 13:37:47 -0700

    Dog structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - structure called dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
