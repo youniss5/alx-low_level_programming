a11059be940865986d0fd3b8bfb26d4aa4754dba
youniss5 <ahmedhyouniss@gmail.com>
2023-07-20 10:00:51 -0700

    print string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ return;
 }
 va_start(ap, n);
 while (i--)
-printf("%s%s", (str = va_arg(ap, char *)) ? str : "(nill)"
+printf("%s%s", (str = va_arg(ap, char *)) ? str : "(nil)"
 , i ? (separator ? separator : "") : "\n");
 va_end(ap);
 }
