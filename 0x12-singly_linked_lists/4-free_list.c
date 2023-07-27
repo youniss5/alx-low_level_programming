7d54aebb915d8831f18182eb47f93edf4ddafc85
youniss5 <ahmedhyouniss@gmail.com>
2023-07-27 14:19:06 -0700

    free space

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stddef.h>
+/**
+ * free_list - free nodes of the list
+ * @head: head node pointer
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+list_t *node, *next_node;
+
+if (!head)
+return;
+node = head;
+while (node)
+{
+next_node = node->next;
+free(node->str);
+free(node);
+node = next_node;
+}
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <stddef.h>
 /**
 * struct list_s - singly linked list
 * @str: string - (malloc'ed string)
