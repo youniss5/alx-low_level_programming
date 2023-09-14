41bd013c8b0be7a234ed40287c8e310eacb98aee
youniss5 <ahmedhyouniss@gmail.com>
2023-09-14 10:31:27 -0700

    print dlistint

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_dlistint -  function that prints all the elements of a list
+ * @head: head node address
+ * Return: size of the list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+size_t x = 0;
+
+while (h)
+{
+printf("%d\n", h->n);
+h = h->next;
+x++;
+}
+return (x);
+}
